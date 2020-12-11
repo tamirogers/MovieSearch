991b22d7e55fc2c90d874fa3c34e37417820f1bb
Rogers <troger05@txt.textron.com>
2020-12-11 11:42:21 -0600

    setUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './home/movies/movies.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent, pathMatch: 'full' },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
